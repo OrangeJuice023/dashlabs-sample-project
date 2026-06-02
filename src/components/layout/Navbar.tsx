3da35d95500e725df856ee67e3b9be3e408493b3
Gervi Paulo C. Corado <87125976+OrangeJuice023@users.noreply.github.com>
2026-06-02 17:37:47 +0800

    Update Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -30,7 +30,7 @@ export function Navbar() {
         }
       `}
     >
-      <div className="max-w-[1200px] mx-auto px-6 h-16 flex items-center justify-between">
+      <div className="max-w-[1200px] mx-auto px-8 md:px-12 lg:px-16 h-16 flex items-center justify-between">
 
         {/* Logo */}
         <Link
