7fb34c80fde9e8910b7a842d0485ec83824fd808
Gervi Paulo C. Corado <87125976+OrangeJuice023@users.noreply.github.com>
2026-06-05 09:38:55 +0800

    Add SyntheticBadge component to HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { SyntheticBadge } from "@/components/SyntheticBadge";
 import { Hero } from "@/components/home/Hero";
 import { WhyShowcase } from "@/components/home/WhyShowcase";
 import { DatasetArchitecture } from "@/components/home/DatasetArchitecture";
@@ -6,6 +7,7 @@ import { ProjectGrid } from "@/components/home/ProjectGrid";
 export default function HomePage() {
   return (
     <>
+      <SyntheticBadge variant="banner" />
       <Hero />
       <WhyShowcase />
       <DatasetArchitecture />
